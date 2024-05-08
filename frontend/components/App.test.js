62ed906569f9d73e0c4daabb3c298ac8b1c78af2
Paul Maloney <paul@paulmaloney.com>
2024-05-08 09:51:29 -0700

    completed project

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -2,76 +2,52 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import App from './App'
+import txt from '../i18n/index.json'
+
 
 describe('Module 4 Project Tests', () => {
-  describe('English Language', () => {
-    /*
-      👉 TASK 1
-
-      One test is done for you as an example.
-    */
-    test(`TEXT_HEADING_CREATE_ACCOUNT is visible`, () => {
-      render(<App lang="en" />)
-      expect(screen.getByText("Create an Account")).toBeVisible()
-    })
-  })
-  describe('Spanish Language', () => {
-    /*
-      👉 TASK 3
-
-      This is done after making the UI multilingual.
-    */
-  })
-  describe('getEntriesByKeyPrefix', () => {
-    test('can extract the correct data', () => {
-    /*
-      👉 TASK 4 part 2
-
-      Implement the function `getEntriesByKeyPrefix` below
-      and then come back here and write a few tests
-      to ensure it works as expected.
-
-      Although it should be noted that commonly,
-      the tests are written _before_ implementing
-      the function being tested.
-    */
+  const allLanguages = [["en", "English Language"], ["esp", "Spanish Language"]]
+  allLanguages.forEach(lang => {
+    const allTexts = getEntriesByKeyPrefix(txt[lang[0]], "TEXT")
+    const allLabels = getEntriesByKeyPrefix(txt[lang[0]], "LABEL")
+    const allPlaceholders = getEntriesByKeyPrefix(txt[lang[0]], "PLACEHOLDER")
+    console.log(allTexts)
+    console.log(allLabels)
+    console.log(allPlaceholders)
+    describe(lang[0], () => {
+      /*
+        👉 TASK 1
+  
+        One test is done for you as an example.
+      */
+     console.log(allTexts)
+      allTexts.forEach(txt => {
+          test(`${txt[0]} is visible`, () => {
+            render(<App lang={lang[0]} />)
+            expect(screen.getByText(txt[1])).toBeVisible()
+          })
+        })
+    
+      allLabels.forEach(lbl => {
+          test(`${lbl[0]} is visible`, () => {
+            render(<App lang={lang[0]} />)
+            expect(screen.getByLabelText(lbl[1])).toBeVisible()
+          })
+        })
+    
+      allPlaceholders.forEach(plc => {
+          test(`${plc[0]} is visible`, () => {
+            render(<App lang={lang[0]} />)
+            expect(screen.getByPlaceholderText(plc[1])).toBeVisible()
+          })
+        })
+    
+        
+  
     })
   })
 })
-function getEntriesByKeyPrefix(obj, keyPrefix) {
-  /*
-    👉 TASK 4 part 1
 
-    Implement a function that takes as first argument an object `obj` such as this:
-
-    {
-      abc_1: "data_abc_1",
-      abc_2: "data_abc_2",
-      xyz_1: "data_xyz_1",
-      abc_3: "data_abc_3",
-    }
-
-    and takes as second argument a string `keyPrefix` such as this: "abc"
-
-    and returns an array of arrays such as this (for the arguments given in the examples above):
-
-    [
-      ["abc_1", "data_abc_1"],
-      ["abc_2", "data_abc_2"],
-      ["abc_3", "data_abc_3"],
-    ]
-
-    If the function is passed the same `obj` as above but a `keyPrefix` of "xyz" then it would return:
-
-    [
-      ["xyz_1", "data_xyz_1"],
-    ]
-
-    If the function is passed the same `obj` as above but a `keyPrefix` of "foo" then it would return the empty array.
-
-    The function looks inside the object `obj`, finds all properties whose property names begin
-    with the `keyPrefix` given (followed by an underscore), and reorganizes the information before returning it.
-    The properties that match the `keyPrefix` are returned inside an array holding key-value-pair sub-arrays.
-
-  */
+function getEntriesByKeyPrefix (obj, keyPrefix) {
+  return Object.entries(obj).filter(([key]) => key.split("_")[0] === keyPrefix)
 }
